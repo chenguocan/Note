50163bb88e89adbfb3642f1407c0793f0578138b
chenguocan <2622870670@qq.com>
2020-10-08 08:52:22 +0800

    编辑对话框

--- src/views/NoteList.vue
+++ src/views/NoteList.vue
@@ -1,23 +1,6 @@
 <template>
   <div class="noteBooks">
     <el-button class="addNewNote" type="text" @click="changeVisible(true)">+新建笔记本</el-button>
-    <el-dialog
-        title="新建笔记本"
-        :visible.sync="noteDialogVisible"
-        width="50%"
-        center>
-      <el-form :model="addNote" label-width="100px">
-        <el-form-item label="笔记本名称">
-          <el-input v-model="addNote.title"></el-input>
-        </el-form-item>
-      </el-form>
-
-      <span slot="footer" class="dialog-footer">
-        <el-button @click="changeVisible(false)">取 消</el-button>
-        <el-button type="primary" @click="submitNote">确 定</el-button>
-      </span>
-    </el-dialog>
-
     <hr/>
     <h5 class="title">笔记本列表({{ noteList.length }})</h5>
     <div class="booksBox">
@@ -29,10 +12,44 @@
               }}<span>({{ item.noteCounts }})</span></i>
             <div class="btn">
               <el-button type="text delete" @click="deleteNote(item.id)">删除</el-button>
-              <el-button type="text">编辑</el-button>
+              <el-button type="text" @click="changeEditVisible(true)">编辑</el-button>
             </div>
           </li>
         </ul>
+
+        <!--对话框-->
+        <el-dialog
+            title="新建笔记本"
+            :visible.sync="noteDialogVisible"
+            width="50%"
+            center>
+          <el-form :model="addNote" label-width="100px">
+            <el-form-item label="笔记本名称">
+              <el-input v-model="addNote.title"></el-input>
+            </el-form-item>
+          </el-form>
+
+          <span slot="footer" class="dialog-footer">
+        <el-button @click="changeVisible(false)">取 消</el-button>
+        <el-button type="primary" @click="submitNote">确 定</el-button>
+      </span>
+        </el-dialog>
+
+        <el-dialog
+            title="编辑笔记本"
+            :visible.sync="editDialogVisible"
+            width="50%"
+            center>
+          <el-form :model="addNote" label-width="100px">
+            <el-form-item label="笔记本名称">
+              <el-input v-model="addNote.title"></el-input>
+            </el-form-item>
+          </el-form>
+          <span slot="footer" class="dialog-footer">
+                   <el-button @click="changeEditVisible(false)">取 消</el-button>
+                   <el-button type="primary"  @click="edit">确 定</el-button>
+                </span>
+        </el-dialog>
       </div>
     </div>
   </div>
@@ -46,6 +63,7 @@ export default {
       noteList: [],
       trashList: [],
       noteDialogVisible: false,
+      editDialogVisible: false,
       addNote: {
         title: '',
       },
@@ -87,7 +105,7 @@ export default {
     },
     async deleteNote(id) {
       const res = await this.$api.deleteNote(id).catch(() => {
-        return window.alert('笔记本不为空或者回收站中还有属于当前笔记本的笔记');
+            return window.alert('笔记本不为空或者回收站中还有属于当前笔记本的笔记');
           }
       );
       if (res) {
@@ -96,6 +114,12 @@ export default {
         }
         await this.getNoteList();
       }
+    },
+    changeEditVisible(visible) {
+      this.editDialogVisible = visible;
+    },
+    edit(){
+
     }
   },
   created() {
@@ -129,11 +153,11 @@ export default {
     margin-left: auto;
     margin-right: auto;
     overflow: hidden;
+
     .noteBooksList {
       min-width: 700px;
       height: 100%;
       background: white;
-      border-radius: 5px;
       overflow: auto;
 
       ul {
@@ -145,6 +169,7 @@ export default {
           align-items: center;
           border-bottom: 1px solid rgb(179, 192, 200);
           justify-content: space-between;
+
           i {
             cursor: pointer;
           }
